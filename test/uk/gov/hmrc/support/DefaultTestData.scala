35b01a028360edd03970b4dffe056f08cdabe8b5
Dimitri Theulings <dimitri@tastypies.co.uk>
2017-06-19 13:30:37 +0100

    Allow multiple platforms to be configured at the same time

--- test/uk/gov/hmrc/support/DefaultTestData.scala
+++ test/uk/gov/hmrc/support/DefaultTestData.scala
@@ -21,7 +21,8 @@ import java.util.UUID
 import play.api.Configuration
 import play.api.test.FakeRequest
 import uk.gov.hmrc.snsclient.aws.sns.SnsConfiguration
-import uk.gov.hmrc.snsclient.model.{Endpoint, Notification}
+import uk.gov.hmrc.snsclient.model.NativeOS.{Android, Ios, Windows}
+import uk.gov.hmrc.snsclient.model.{Endpoint, NativeOS, Notification}
 
 trait DefaultTestData {
 
@@ -30,11 +31,12 @@ trait DefaultTestData {
   def postSnsRequest(url: String) = FakeRequest("POST", url).withHeaders("Content-Type" -> "application/json", "Accept" -> "application/vnd.hmrc.1.0+json")
 
   val defaultConfig = Map(
-    gcmOsKey -> "android",
-    gcmApiKey -> "test-apiKey",
     awsAccessKey -> "test-accessKey",
     awsSecretKey -> "test-secret",
-    gcmApplicaitonArnKey -> "test-applicationArn",
+    apiKey.replace("PLATFORM", Android) -> "test-apiKey",
+    applicationArnKey.replace("PLATFORM", Android) -> "android-test-applicationArn",
+    apiKey.replace("PLATFORM", Windows) -> "test-apiKey",
+    applicationArnKey.replace("PLATFORM", Windows) -> "windows-test-applicationArn",
     awsRegionKey -> "eu-west-1",
     awsRegionOverrideKey -> "stubbed-aws-sns",
     awsStubbingKey -> true
@@ -42,15 +44,16 @@ trait DefaultTestData {
 
   val defaultSnsConfiguration = new SnsConfiguration(Configuration from defaultConfig)
 
-  val windowsNotification : Notification = simpleNotification(UUID.randomUUID().toString, "windows")
-  val windowsNotificationWithMessageId : Notification = notificationWithMessageId(UUID.randomUUID().toString, "windows")
-  val androidNotification : Notification = simpleNotification(UUID.randomUUID().toString, "android")
-  val androidNotificationWithMessageId : Notification = notificationWithMessageId(UUID.randomUUID().toString, "android")
-  val iosNotification : Notification = simpleNotification(UUID.randomUUID().toString, "ios")
-  val iosNotificationWithMessageId : Notification = notificationWithMessageId(UUID.randomUUID().toString, "ios")
+  val windowsNotification: Notification = simpleNotification(UUID.randomUUID().toString, Windows)
+  val windowsNotificationWithMessageId: Notification = notificationWithMessageId(UUID.randomUUID().toString, Windows)
+  val androidNotification: Notification = simpleNotification(UUID.randomUUID().toString, Android)
+  val androidNotificationWithMessageId: Notification = notificationWithMessageId(UUID.randomUUID().toString, Android)
+  val iosNotification: Notification = simpleNotification(UUID.randomUUID().toString, Ios)
+  val iosNotificationWithMessageId: Notification = notificationWithMessageId(UUID.randomUUID().toString, Ios)
 
-  def simpleNotification(id:String, os: String) = Notification(id, "registrationToken", os, "Tax is fun!", None)
-  def notificationWithMessageId(id:String, os: String) = Notification(id, "registrationToken", os, "Tax is fun!", Option("123"))
+  def simpleNotification(id: String, os: String) = Notification(id, "registrationToken", os, "Tax is fun!", None)
 
-  val androidEndpoint = Endpoint(defaultSnsConfiguration.gcmConfiguration.get.osName, "android-registration-token")
+  def notificationWithMessageId(id: String, os: String) = Notification(id, "registrationToken", os, "Tax is fun!", Option("123"))
+
+  val androidEndpoint = Endpoint(Android, "android-registration-token")
 }
